70df9991bb95cf2d49ceb3035ba76ff27fb17992
chetan gupta <chetangupta123raj@gmail.com>
2024-01-25 19:55:51 +0530

    feat: added search api route added user search functionality

---
+++ src/app/api/users/search/route.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from "next/server";
+import prisma from "@/app/lib/prisma";
+import { getUserSession } from "@/app/lib/auth";
+
+export async function GET(request: Request) {
+  const { id } = await getUserSession();
+  const { searchParams } = new URL(request.url);
+  const query = searchParams.get("query") || "";
+  const users = await prisma.user.findMany({
+    where: {
+      OR: [
+        {
+          name: {
+            contains: query,
+          },
+        },
+        {
+          email: {
+            contains: query,
+          },
+        },
+      ],
+      NOT: [
+        {
+          id,
+        },
+        {
+          account: null,
+        },
+      ],
+    },
+  });
+  return NextResponse.json(users);
+}
